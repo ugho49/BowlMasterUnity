b448c1f15bd7804fd6fdf5ef5daa5ecb5a59c127
Ugho STEPHAN <ugho.stephan@consertotech.pro>
2017-08-30 17:05:34 +0200

    WIP en of course 192

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 launchVelocity;
+
+	private Rigidbody rigidBody;
+	private AudioSource audioSource;
+
+	void Awake() {
+		audioSource = GetComponent<AudioSource> ();
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		Launch ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Launch()
+	{
+		rigidBody.velocity = launchVelocity;
+		audioSource.Play ();
+	}
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public GameObject ball;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.z > 1829f) {
+			return;
+		}
+
+		transform.position = ball.transform.position + offset;
+	}
+}
