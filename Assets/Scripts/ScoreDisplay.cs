333dd99b6e4b6a04d44adb57f512c33cf52c3f15
Ugho STEPHAN <ugho.stephan@consertotech.pro>
2017-09-04 16:24:45 +0200

    WIP end of course 225

---
+++ Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+	public Text[] rolls, frames;
+
+	// Use this for initialization
+	void Start () {
+		rolls [0].text = "X";
+		frames [0].text = "0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
